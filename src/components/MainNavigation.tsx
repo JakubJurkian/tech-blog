d52838a5875d692b1d19912e84c2d75e0f55802f
Jakub <kuba.jur03@gmail.com>
2023-06-03 13:56:36 +0200

    added main-navigation

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,17 +1,21 @@
 import { NavLink } from 'react-router-dom';
 
+import classes from './MainNavigation.module.css';
+
 const MainNavigation = () => {
   return (
-    <header className="">
+    <header className={`${classes['nav-bg']} fixed top-0 w-full`}>
       <nav>
-        <ul className="">
+        <ul className="flex space-x-2 p-4">
           <li>
-            <NavLink to="/" className="">
+            <NavLink to="/" className="text-lg">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/auth">Login/Register</NavLink>
+            <NavLink to="/auth" className="text-lg">
+              Login/Register
+            </NavLink>
           </li>
         </ul>
       </nav>
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,15 +2,19 @@ import { Outlet } from 'react-router-dom';
 
 import bgPhoto from '/bg-photo.webp';
 
-import MainNavigation from "../components/MainNavigation";
+import MainNavigation from '../components/MainNavigation';
 const RootLayout = () => {
   return (
     <>
-      <MainNavigation />
-      <div>
-        <img src={bgPhoto} alt="background-photo" className="max-h-96 w-full blur-sm" />
+      <div className="relative">
+        <img
+          src={bgPhoto}
+          alt="background-photo"
+          className="max-h-96 w-full blur-sm"
+        />
+        <MainNavigation />
       </div>
-      <main>
+      <main >
         <Outlet />
       </main>
     </>
