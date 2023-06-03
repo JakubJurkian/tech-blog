907dcb0fc27ee050c05004e73a1c258ebe0355e1
Jakub <kuba.jur03@gmail.com>
2023-06-03 14:26:39 +0200

    created & designed 'me' card in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,27 @@
+import classes from './Home.module.css';
+
 function HomePage() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <div className="flex flex-col relative">
+      <div
+        className={`${classes['bg-card']} flex p-6 m-auto rounded-xl shadow-md relative bottom-32`}
+      >
+        <img
+          src="./me.jpg"
+          alt=""
+          width="148"
+          height="148"
+          className="rounded-lg mr-8"
+        />
+        <div>
+          <h2 className='text-2xl'>Jakub Jurkian</h2>
+          <p>...description</p>
+        </div>
+      </div>
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    </div>
   );
 }
 
