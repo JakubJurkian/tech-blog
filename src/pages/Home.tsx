fecf5bfbcbf1d4e99cc64a68998e430c435c9923
Jakub <kuba.jur03@gmail.com>
2023-06-04 20:10:23 +0200

    designed main card in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,18 +4,26 @@ function HomePage() {
   return (
     <div className="flex flex-col relative">
       <div
-        className={`${classes['bg-card']} flex p-6 m-auto rounded-xl shadow-md relative bottom-32`}
+        className={`${classes['bg-card']} flex py-4 px-3 xxs:px-5 xs:p-5 xxs:bottom-20 xs:bottom-28 sm:bottom-36 sm:px-8 md:w-3/5 max-w-lg m-auto rounded-xl shadow-md relative bottom-16`}
       >
         <img
           src="./me.jpg"
           alt=""
           width="148"
           height="148"
-          className="rounded-lg mr-8"
+          className="rounded-lg mr-6 xxs:mr-8 xs:w-40 sm:w-44"
         />
-        <div>
-          <h2 className='text-2xl'>Jakub Jurkian</h2>
+        <div className="text-center w-full">
+          <h2 className="text-xl xs:text-2xl ">Jakub Jurkian</h2>
+          <a
+            target="_blank"
+            href="https://github.com/JakubJurkian"
+            className="text-blue-500 font-medium align-middle"
+          >
+            GITHUB
+          </a>
           <p>...description</p>
+          {/* github etc icons and links */}
         </div>
       </div>
       <div>
